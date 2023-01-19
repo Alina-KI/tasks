7bffa81dc9c02583991240ba422a1b5247b70d35
Alina-KI <kraynova.alina.2002@mail.ru>
2023-01-19 13:21:35 +0300

    solved stocks

--- stocks.ts
+++ stocks.ts
@@ -1,19 +1,24 @@
-let prices1: number[] = [7, 1, 5, 3, 6, 4]
-let prices2: number[] = [7, 6, 4, 3, 1]
+let prices1: number[] = [7, 1, 5, 3, 6, 4]//7
+let prices2: number[] = [7, 6, 4, 3, 1]//0
 
 function maxProfit(prices: number[]) {
-    const i = prices.slice(0, prices.length - 1).reduceRight((acc, price) => {
-        return acc - price
-    }, prices[prices.length - 1])
-    if (i < 0){
+    let currentPrice = prices[0]
+    const profit = []
+    const array = prices.slice(1, prices.length - 1).map(item => {
+        const newItem = item > currentPrice
+        if (newItem)
+            profit.push(item - currentPrice)
+        currentPrice = item
+        return newItem
+    })
+    if (array.every(item => !item)) {
         return 0
-    }
-    else {
-        return prices.reduce((acc) => {
-            return acc
+    } else {
+        return profit.reduce((acc, item) => {
+            return acc + item
         }, 0)
     }
 }
 
 console.log(maxProfit(prices1))
-// console.log(maxProfit(prices2))
+console.log(maxProfit(prices2))
